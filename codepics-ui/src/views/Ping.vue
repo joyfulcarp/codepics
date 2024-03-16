610f677a641c66fb571bef0619a60b04f468bc87
Daniel Gao <daniel.gao.work@gmail.com>
2024-03-16 14:35:34 -0400

    Ping backend with axios

--- codepics-ui/src/views/Ping.vue
+++ codepics-ui/src/views/Ping.vue
@@ -1,16 +1,34 @@
 <template>
   <div>
-    <p>{{ msg }}</p>
+    <p>ping... {{ msg }}</p>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'PingView',
   data() {
     return {
-      msg: 'Hello!'
+      msg: ''
     }
+  },
+  methods: {
+    ping() {
+      const path = 'http://localhost:5001/ping'
+      axios
+        .get(path)
+        .then((res) => {
+          this.msg = res.data
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    }
+  },
+  created() {
+    this.ping()
   }
 }
 </script>
