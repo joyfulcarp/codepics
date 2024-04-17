7785c97d38818f434987ef455e6046750d8cba54
Daniel Gao <daniel.gao.work@gmail.com>
2024-04-17 10:45:08 -0400

    Add backend requirements.txt

--- run.sh
+++ run.sh
@@ -8,6 +8,9 @@ echo "VITE_BACKEND_URL=http://$IP_ADDRESS:$BACKEND_PORT/" > codepics-ui/.env.pro
 
 echo "Run:"
 echo "source venv/bin/activate"
-echo "cd server && flask run --host=0.0.0.0 --port=$BACKEND_PORT"
+echo "cd server"
+echo "pip install -r requirements.txt"
+echo "flask run --host=0.0.0.0 --port=$BACKEND_PORT"
+echo
 echo "cd codepics-ui && npm run build-only "
 echo "npm run preview -- --host=0.0.0.0 --port=$FRONTEND_PORT"
